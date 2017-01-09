2c2a81e6358d7dcb100a8cb8655e01a512eafaf8
champon@macpro <735037139@qq.com>
2017-01-10 00:44:14 +0800

    duty

--- createDailyDuty.sh
+++ createDailyDuty.sh
@@ -1,3 +1,4 @@
 FILENAME="duty_`date '+%Y.%m.%d'`.md"
 touch ${FILENAME}
-
+#KKK="test_`date '+%Y.%m.%d-%H%M'`.txt"
+#touch ${KKK};
---
+++ crontab_push.sh
@@ -0,0 +1,4 @@
+#KKK="test_`date '+%Y.%m.%d-%H%M'`.txt"
+#cd ~/github/todolist/
+#touch ${KKK}
+git push origin master
